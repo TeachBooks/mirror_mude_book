cd22497ee35c67c367f7c141c723298562674f4c
sheepmax <guichardmax@gmail.com>
2023-08-21 17:32:05 +0200

    Added some errors for when cells are not run

--- book/cookbook/grading.py
+++ book/cookbook/grading.py
@@ -7,7 +7,10 @@ def check(f):
 		button = widgets.Button(description="Check answer")
 		@output.capture(clear_output=True,wait=True)
 		def _inner_check(button):
-			f(*args, **kwargs)
+			try:	
+				f(*args, **kwargs)
+			except:
+				print("Something went wrong, have you filled all the functions and run the cells?")
 		button.on_click(_inner_check)
 		display(button, output)
 	return wrapper
